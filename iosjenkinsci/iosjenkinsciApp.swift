7fbd8093242be62c4d2451ded3fa61f5fc38d41a
kimtaeho <>
2021-01-11 14:57:00 +0900

    saple

---
+++ iosjenkinsci/iosjenkinsciApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosjenkinsciApp.swift
+//  iosjenkinsci
+//
+//  Created by kimtaeho on 2021/01/11.
+//
+
+import SwiftUI
+
+@main
+struct iosjenkinsciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
